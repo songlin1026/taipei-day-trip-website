35f6c7ecf7ca0a2d2456555ea77da9470647e1cf
SongLin <songlin1026@gmail.com>
2021-05-11 18:48:16 +0800

    debug signup api

--- app.py
+++ app.py
@@ -261,5 +261,5 @@ def attractionId(attractionId):
 		return {"error": True,"message": "伺服器發生錯誤"}
 
 
-app.run(port=3000)
-# app.run(host="0.0.0.0",port=3000)
+# app.run(port=3000)
+app.run(host="0.0.0.0",port=3000)
